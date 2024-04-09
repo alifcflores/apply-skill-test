425b76acadda1fd5773e39b0b95b937053981904
Alif Corrêa Flores <alifcorrea.qa@gmail.com>
2024-04-09 09:11:46 -0300

    config env params

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,10 @@ module.exports = defineConfig({
     baseUrl: 'https://automationexercise.com',
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
-      
     },
   },
+  env: {
+    USER_EMAIL:'Mariana.Kohler@orderly-airfield.com',
+    USER_PASSWORD:'nnMkPmvc779BnbWKHki7'
+  }
 });
